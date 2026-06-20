5ee96b04da9ed0276dbb890db86510681f823df7
Mike Harder <mharder@microsoft.com>
2026-06-19 21:11:29 -0700

    add eslint

--- action3/script.js
+++ action3/script.js
@@ -1,4 +1,10 @@
 export default async function doStuff({github, context}) {
+  const { data: repo } = await github.rest.repos.get({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+  });
+  console.log(`Event ${context.eventName} on ${repo.full_name}`);
+
   return "hello from script.js";
   // return context.payload.client_payload.value;
 }
